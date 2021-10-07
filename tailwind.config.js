3448b58e298e068943da7f4fe59d16b66c62dd4b
Esteban Palacio <estipalacio@gmail.com>
2021-10-07 18:56:44 -0400

    Added colours and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkGreen': '#002529',
+        'sacramentoGreen': '#012F34',
+        'jungleGreen': '#004047',
+        'midnightGreen': '#014E56',
+        'policeBlue': '#2C6269',
+        'raptureBlue': '#79C8C7',
+        'lightCoral': '#F67E7E',
+        'white': '#ffffff'
+      }
+    },
   },
   variants: {
     extend: {},
