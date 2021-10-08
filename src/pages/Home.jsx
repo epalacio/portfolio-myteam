cf08be9046aaf95be7b13ec9fa1dfb9471b15cf1
Esteban Palacio <estipalacio@gmail.com>
2021-10-08 15:48:06 -0400

    added content and styling for banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 import Hero from '../components/Hero.component'
 import Features from '../components/Features.component'
 import Stories from '../components/Stories.component';
+import StartedBanner from '../components/StartedBanner.component';
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Hero />
             <Features />
             <Stories />
+            <StartedBanner />
         </Fragment>
     )
 }
